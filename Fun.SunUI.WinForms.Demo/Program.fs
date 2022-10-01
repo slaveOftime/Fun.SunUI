bfc79390a6531c973ac68b2d3e8a8ecdac8d5436
binwen <albertwoo_slaveoftime@hotmail.com>
2022-10-01 20:26:58 +0800

    WIP

--- Fun.SunUI.WinForms.Demo/Program.fs
+++ Fun.SunUI.WinForms.Demo/Program.fs
@@ -1,10 +1,35 @@
 ﻿module Program
 
 open System
+open System.Windows.Forms
 open FSharp.Data.Adaptive
+open Microsoft.Extensions.DependencyInjection
+open Fun.SunUI.WinForms
+
 
 [<EntryPoint; STAThread>]
 let main (args: string[]) =
     let count = cval 0
+
+    let mainForm =
+        Form'() {
+            Controls [
+                FlowLayoutPanel'() {
+                    Controls [
+                        Label'() { Text(count |> AVal.map (sprintf "Count = %d")) }
+                        Button'() {
+                            Click(fun _ _ -> transact (fun _ -> count.Value <- count.Value + 1))
+                            Text "Incease"
+                        }
+                    ]
+                }
+            ]
+        }
+
+    let services = new ServiceCollection()
+    let sp = services.BuildServiceProvider()
+    let nativeMainForm = mainForm.CreateOrUpdate(sp, ValueNone).NativeElement :?> Form
+
+    Application.Run(nativeMainForm)
 
     0
